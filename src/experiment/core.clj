e10e73003ebbc5118076a53bb00a891f65bfa908
Ajay Ramanathan <ajay.010293@gmail.com>
2014-03-10 08:49:00 +0530

    Initial commit

---
+++ src/experiment/core.clj
@@ -0,0 +1,1 @@
+(ns experiment.core)
---
+++ test/experiment/test/core.clj
@@ -0,0 +1,6 @@
+(ns experiment.test.core
+  (:use [experiment.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
