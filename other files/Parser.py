016225872a73c236cff025a716fd9e34441f0f64
Ajay Ramanathan <ajay.010293@gmail.com>
2014-03-17 17:26:53 +0530

    Changing language to python

---
+++ other files/Parser.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+#The MIT License (MIT)
+
+#Copyright (c) 2014 Ajay Ramanathan
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+
+'''!$$SOF
+
+EMAIL_ADDR  = ' '
+COMMAND= '010' '199' '299' '399' '499' '599' '699' '799' '899' '999' '301'
+OBJ_DATA    = 'NO'
+MAKE_EPHEM  = 'YES'
+TABLE_TYPE  = 'VECTORS'
+CENTER      = '500@0'
+REF_PLANE   = 'ECLIPTIC'
+COORD_TYPE  = 'GEODETIC'
+START_TIME  = '2014-JAN-1'
+STOP_TIME   = '2016-JAN-1'
+STEP_SIZE   = '60 Min'
+REF_SYSTEM  = 'J2000'
+OUT_UNITS   = 'KM-S'
+CSV_FORMAT  = 'YES'
+
+!$$EOF'''
+
+#Jan-2014 to Jan-2016;For all Planets, Sun, and Moon;
+#Step Size 1 Hour
